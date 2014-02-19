319c1b078ae862c4df90710802b1916975d906e7
evapandulova <pandulova@gmail.com>
2014-02-19 17:59:18 +0100

    Create new graphics, did_shake_phone()

--- js/main.js
+++ js/main.js
@@ -2,34 +2,65 @@
 var lastAccX = 0,
     lastAccY = 0,
     lastAccZ = 0,
-    sensitivity = 20,
+    sensitivity = 70,
+    counter = 0,
 
-    did_shake_phone = function () {
+
+    did_shake_phone = function (toTheLeft) {
         'use strict';
-        var elems = document.getElementsByClassName('btn');
-        for (var i=0;i<elems.length;i+=1){
-          elems[i].style.display = 'none';
-        document.getElementsByClassName('txt')[0].style.display = "none";
-        document.getElementById('btn5').style.display = "block";
-        document.getElementById('btn6').style.display = "block";
-        document.getElementById('btn17').style.display = "block";
-        }
+        var elems = document.getElementsByClassName('btn'),
+            i,
+            factor;
 
+        console.log(toTheLeft);
+
+        /*if (toTheLeft) {
+            factor = -1;
+        } else {
+            factor = 1;
+        }*/
+
+        if (counter === 0) {
+            for (i = 0; i < elems.length; i = i + 1) {
+            elems[i].style.left = factor + '10%';
+            document.getElementById('btn1f').style.display = 'block';
+
+            document.getElementsByClassName('text')[0].style.display = 'none';
+            document.getElementsByClassName('text-try')[0].style.display = 'block';
+            }
+        }
+        
+
+        else if (counter === 1)
+            for (i = 0; i < elems.length; i = i + 1) {
+            elems[i].style.left = factor + '10%';
+
+            document.getElementById('btn2f').style.display = 'block';
+
+            document.getElementsByClassName('text')[0].style.display = 'none';
+            document.getElementsByClassName('text-try')[0].style.display = 'block';
+            }
+
+        counter = counter + 1;
     };
-    window.addEventListener('devicemotion', function (event) {
+
+window.addEventListener('devicemotion', function (event) {
     'use strict';
     var acc = event.accelerationIncludingGravity,
         x = acc.x,
         y = acc.y,
         z = acc.z,
-        delta = Math.abs(x - lastAccX + y - lastAccY + y - lastAccZ);
+        delta = Math.abs(x - lastAccX + y - lastAccY + y - lastAccZ),
+        toTheLeft = false;
 
-    console.log(delta);
+    if (x < 0) {
+        toTheLeft = true;
+    }
 
     if (delta > sensitivity) {
-
-        did_shake_phone();
+        did_shake_phone(toTheLeft);
     }
+
     lastAccX = x;
     lastAccY = y;
     lastAccZ = z;

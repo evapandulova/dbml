8715d9f62532f44147e09b771508f23cda4e82f4
evapandulova <pandulova@gmail.com>
2014-03-03 14:35:54 +0100

    Create Collision

--- js/main-lupe.js
+++ js/main-lupe.js
@@ -1,35 +1,35 @@
 /*jslint browser:true, devel:true*/
-var magnifierEl = document.getElementById('magnifier'),
-    lastAccX = 0,
-    lastAccY = 0,
-    lastAccZ = 0,
+// var magnifierEl = document.getElementById('magnifier'),
+//     lastAccX = 0,
+//     lastAccY = 0,
+//     lastAccZ = 0,
 
 
 
-    lupe = function () {
-        'use strict';
-        var that = {},
-            move = function (x, y) {
-                magnifierEl.style.left = x + 'px';
-                magnifierEl.style.top = y + 'px';
-            };
+//     lupe = function () {
+//         'use strict';
+//         var that = {},
+//             move = function (x, y) {
+//                 magnifierEl.style.left = x + 'px';
+//                 magnifierEl.style.top = y + 'px';
+//             };
 
-        that.move = move;
-        return that;
-    },
+//         that.move = move;
+//         return that;
+//     },
 
 
 
-    myLupe = lupe(),
+//     myLupe = lupe(),
 
-    did_drag_lupe = function (event) {
-        'use strict';
-        var firstTouch = event.touches[0],
-            x = firstTouch.clientX - 100,
-            y = firstTouch.clientY - 100;
+//     did_drag_lupe = function (event) {
+//         'use strict';
+//         var firstTouch = event.touches[0],
+//             x = firstTouch.clientX - 100,
+//             y = firstTouch.clientY - 100;
 
-        myLupe.move(x, y);
-    };
+//         myLupe.move(x, y);
+//     };
 
   
     show_last_frame = function () {
@@ -40,13 +40,13 @@ var magnifierEl = document.getElementById('magnifier'),
         document.getElementById("btn-buchen").style.visibility = "visible";
         document.getElementById("train").style.visibility = "visible";
     }
-    setTimeout("show_last_frame()", 10000);  
+    // setTimeout("show_last_frame()", 10000);  
 
 
 
-magnifierEl.addEventListener('touchmove', did_drag_lupe);
-document.addEventListener('touchmove', function (event) {
-    'use strict';
-    event.preventDefault();
-});
+// magnifierEl.addEventListener('touchmove', did_drag_lupe);
+// document.addEventListener('touchmove', function (event) {
+//     'use strict';
+//     event.preventDefault();
+// });
 
--- js/test.js
+++ js/test.js
@@ -1,118 +1,82 @@
-        // window.addEventListener('load', function(){
-         
-        //  var box1 = document.getElementById('box1')
-        //  var statusdiv = document.getElementById('statusdiv')
-        //  var startx = 0
-        //  var dist = 0
-         
-         
-        //  box1.addEventListener('touchstart', function(e){
-        //   var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
-        //   startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
-        //   statusdiv.innerHTML = 'Status: touchstart<br /> ClientX: ' + startx + 'px'
-        //   e.preventDefault()
-        //  }, false)
-         
-        //  box1.addEventListener('touchmove', function(e){
-        //   var touchobj = e.changedTouches[0] // reference first touch point for this event
-        //   var dist = parseInt(touchobj.clientX) - startx
-        //   statusdiv.innerHTML = 'Status: touchmove<br /> Horizontal distance traveled: ' + dist + 'px'
-        //   e.preventDefault()
-        //  }, false)
-         
-        //  box1.addEventListener('touchend', function(e){
-        //   var touchobj = e.changedTouches[0] // reference first touch point for this event
-        //   statusdiv.innerHTML = 'Status: touchend<br /> Resting x coordinate: ' + touchobj.clientX + 'px'
-        //   e.preventDefault()
-        //  }, false)
-         
-        // }, false)
-
-        // function myFunction()
-        // {
-        // var x="";
-        // var time=new Date().getHours();
-        // if (time<20)
-        //   {
-        //   x="Good day";
-        //   }
-        // else
-        //   {
-        //   x="Good evening";
-        //   }
-        // document.getElementById("demo").innerHTML=x;
-        // };
-
-
-// var overlaps = (function () {
-//     function getPositions( elem ) {
-//         var pos, width, height;
-//         pos = $( elem ).position();
-//         width = $( elem ).width();
-//         height = $( elem ).height();
-//         return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-//     }
-
-//     function comparePositions( p1, p2 ) {
-//         var r1, r2;
-//         r1 = p1[0] < p2[0] ? p1 : p2;
-//         r2 = p1[0] < p2[0] ? p2 : p1;
-//         return r1[1] > r2[0] || r1[0] === r2[0];
-//     }
-
-//     return function ( a, b ) {
-//         var pos1 = getPositions( a ),
-//             pos2 = getPositions( b );
-//         return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-//     };
-// })();
-
-// $(function () {
-// 	$( '#hel' ).html( 'hello kitty' );
-//     var area = $( '#glass' )[0],
-//         box = $( '.lol' )[0],
-//         html;
-    
-//     msg = $( area ).children().not( box ).map( function ( i ) {
-//         return '<p>ACHTUNG! overlapping ' + ( i + 1 ) + ' = ' + overlaps( box, this ) + '</p>';
-//     }).get().join( '' );
-
-//     $( '#hel' ).append( msg );
+// $(document).bind("touchstart touchmove", function(e) {
+//   //Disable scrolling by preventing default touch behaviour
+//   e.preventDefault();
+//   var orig = e.originalEvent;
+//   var x = orig.changedTouches[0].pageX;
+//   var y = orig.changedTouches[0].pageY;
+//   // Move a div
+//   $("#div1").css({top:y, left:x });
 // });
 
+show_frame_2 = function () {
+        'use strict';
+        document.getElementById("magnifier").style.visibility = "hidden";
+        document.getElementById("btn1").style.visibility = "hidden";
+        document.getElementById("gluckwunsch").style.visibility = "visible";
 
-        var overlaps = (function () {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = $( elem ).position();
-        width = $( elem ).width();
-        height = $( elem ).height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
-
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
     };
-})();
 
-$(function () {
-    var area = $( '#area' )[0],
-        box = $( '#box0' )[0],
-        html;
-    
-    html = $( area ).children().not( box ).map( function ( i ) {
-        return '<p>Red box + Box ' + ( i + 1 ) + ' = ' + overlaps( box, this ) + '</p>';
-    }).get().join( '' );
+show_frame_3 = function () {
+        'use strict';
+        document.getElementById("gluckwunsch").style.visibility = "hidden";
+        document.getElementById("text-after").style.visibility = "visible";
+        document.getElementById("btn-buchen").style.visibility = "visible";
+        document.getElementById("train").style.visibility = "visible";
+        document.getElementById("magnifier").style.visibility = "hidden";
+        document.getElementById("btn1").style.visibility = "hidden";
+    };
 
-    $( 'body' ).append( html );
+$.fn.draggable = function() {
+  var offset = null;
+  var start = function(e) {
+    var orig = e.originalEvent;
+    var pos = $(this).position();
+    offset = {
+      x: orig.changedTouches[0].pageX - pos.left,
+      y: orig.changedTouches[0].pageY - pos.top
+    };
+  };
+  var moveMe = function(e) {
+    e.preventDefault();
+    var orig = e.originalEvent;
+    $(this).css({
+      top: orig.changedTouches[0].pageY - offset.y,
+      left: orig.changedTouches[0].pageX - offset.x
+    });
+  };
+  this.bind("touchstart", start);
+  this.bind("touchmove", moveMe);
+};
+$("#div1").draggable();
+
+$(window).load(function(){
+    function collision($div1, $div2) {
+      var x1 = $div1.offset().left;
+      var y1 = $div1.offset().top;
+      var h1 = $div1.outerHeight(true);
+      var w1 = $div1.outerWidth(true);
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = $div2.offset().left;
+      var y2 = $div2.offset().top;
+      var h2 = $div2.outerHeight(true);
+      var w2 = $div2.outerWidth(true);
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
+        
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+      setTimeout("show_frame_2()", 200);  
+      setTimeout("show_frame_3()", 5000); 
+    }
+
+
+window.setInterval(function() {
+    $('#result').text(collision($('#div1'), $('#div2')));
+}, 50);
+
+
+$('#div1,#div2').draggable();
+
+
 });
 
